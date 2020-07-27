9be937c246bc87f22fe353eb74f5375e552b0ee3
Hasif <lavahasif@gmail.com>
2020-07-27 18:18:06 +0530

    Initial commit

---
+++ ConsolePrinter/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Http;
+using printer;
+
+
+namespace ConsolePrinter
+{
+    public class ProductController : ApiController
+    {
+        public Product GetProduct()
+        {
+            
+            new PrintingExample(@"D:\onedrive\Desktop\New Text Document.txt");
+            return new Product()
+            {
+                id = 1,
+                name = "Tea", amt = 100
+            };
+        }
+    }
+    
+    
+}
+
+public class Product
+
+{
+    public int id { get; set; }
+    public String name { get; set; }
+    public int amt { get; set; }
+}
+
---
+++ ConsolePrinter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+
+namespace ConsolePrinter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration(
+                "http://192.168.1.105:60064");
+            config.Routes.MapHttpRoute(
+                name: "API",
+                routeTemplate: "{controller}/{action}/{id}",
+                defaults: new {id = RouteParameter.Optional});
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.Write("Web Api is Started Now.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+        
+        
+        
+        
+        
+        
+        
+        
